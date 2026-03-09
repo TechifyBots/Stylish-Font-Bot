f839ac15db22246a359adc3873da14a7c4b48236
Techify Bots <TechifyBots@gmail.com>
2026-03-09 15:14:22 +0530

    Update callback.py

--- TechifyBots/callback.py
+++ TechifyBots/callback.py
@@ -1,45 +1,49 @@
+import random
 from pyrogram import Client
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from Script import text
-from config import ADMIN
+from config import ADMIN, PICS
 
 @Client.on_callback_query()
 async def callback_query_handler(client, query: CallbackQuery):
-    if query.data == "close":
-        await query.message.delete()
-
-    elif query.data == "back":
-        await query.message.edit_caption(
-            caption=text.START.format(query.from_user.mention),
+    if query.data == "start":
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.START.format(query.from_user.mention)
+            ),
             reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("ℹ️ 𝖠𝖻𝗈𝗎𝗍", callback_data="about"),
-                    InlineKeyboardButton("📚 𝖧𝖾𝗅𝗉", callback_data="help")
-                ],
-                [
-                    InlineKeyboardButton("👨‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 👨‍💻", user_id=int(ADMIN))
-                ]
+                [InlineKeyboardButton('ℹ️ 𝖠𝖻𝗈𝗎𝗍', callback_data='about'),
+                 InlineKeyboardButton('📚 𝖧𝖾𝗅𝗉', callback_data='help')],
+                [InlineKeyboardButton('💬 𝖥𝖾𝖾𝖽𝖻𝖺𝖼𝗄 💬', url='https://telegram.me/TechifySupport')]
             ])
         )
 
-
     elif query.data == "help":
-        await query.message.edit_caption(
-            caption=text.HELP,
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.HELP
+            ),
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("📢 𝖴𝗉𝖽𝖺𝗍𝖾𝗌", url="https://telegram.me/Techifybots"),
-                 InlineKeyboardButton("💬 𝖲𝗎𝗉𝗉𝗈𝗋𝗍", url="https://telegram.me/TechifySupport")],
-                [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄", callback_data="back"),
-                 InlineKeyboardButton("❌ 𝖢𝗅𝗈𝗌𝖾", callback_data="close")]
+                [InlineKeyboardButton('📢 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://telegram.me/Techifybots')],
+                [InlineKeyboardButton('↩️ 𝖡𝖺𝖼𝗄', callback_data="start"),
+                 InlineKeyboardButton('❌ 𝖢𝗅𝗈𝗌𝖾', callback_data="close")]
             ])
         )
 
     elif query.data == "about":
-        await query.message.edit_caption(
-            caption=text.ABOUT,
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.ABOUT
+            ),
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("👨‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 👨‍💻", user_id=int(ADMIN))],
-                [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄", callback_data="back"),
-                 InlineKeyboardButton("❌ 𝖢𝗅𝗈𝗌𝖾", callback_data="close")]
+                [InlineKeyboardButton('💻 𝖱𝖾𝗉𝗈',url='https://github.com/TechifyBots/Stylish-Font-Bot'),
+                 InlineKeyboardButton('👨‍💻 𝖮𝗐𝗇𝖾𝗋',user_id=int(ADMIN))],
+                [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄",callback_data="start")]
             ])
         )
+
+    elif query.data == "close":
+        await query.message.delete()
