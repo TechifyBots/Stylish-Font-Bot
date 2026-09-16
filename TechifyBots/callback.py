771cb3383a00864a0f5cfe81485577e30e0f3472
Techify Bots <TechifyBots@gmail.com>
2026-09-16 15:25:26 +0530

    Update callback.py

--- TechifyBots/callback.py
+++ TechifyBots/callback.py
@@ -1,5 +1,5 @@
 import random
-from pyrogram import Client
+from pyrogram import Client,enums
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from Script import text
 from config import ADMIN, PICS
@@ -15,7 +15,7 @@ async def callback_query_handler(client, query: CallbackQuery):
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('ℹ️ 𝖠𝖻𝗈𝗎𝗍', callback_data='about'),
                  InlineKeyboardButton('📚 𝖧𝖾𝗅𝗉', callback_data='help')],
-                [InlineKeyboardButton('💬 𝖥𝖾𝖾𝖽𝖻𝖺𝖼𝗄 💬', url='https://telegram.me/TechifySupport')]
+                [InlineKeyboardButton('💬 𝖥𝖾𝖾𝖽𝖻𝖺𝖼𝗄 💬', url='https://telegram.me/TechifySupport', style=enums.ButtonStyle.PRIMARY)]
             ])
         )
 
@@ -27,8 +27,8 @@ async def callback_query_handler(client, query: CallbackQuery):
             ),
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('📢 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://telegram.me/Techifybots')],
-                [InlineKeyboardButton('↩️ 𝖡𝖺𝖼𝗄', callback_data="start"),
-                 InlineKeyboardButton('❌ 𝖢𝗅𝗈𝗌𝖾', callback_data="close")]
+                [InlineKeyboardButton('↩️ 𝖡𝖺𝖼𝗄', callback_data="start", style=enums.ButtonStyle.PRIMARY),
+                 InlineKeyboardButton('❌ 𝖢𝗅𝗈𝗌𝖾', callback_data="close", style=enums.ButtonStyle.DANGER)]
             ])
         )
 
@@ -41,7 +41,7 @@ async def callback_query_handler(client, query: CallbackQuery):
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('💻 𝖱𝖾𝗉𝗈',url='https://github.com/TechifyBots/Stylish-Font-Bot'),
                  InlineKeyboardButton('👨‍💻 𝖮𝗐𝗇𝖾𝗋',user_id=int(ADMIN))],
-                [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄",callback_data="start")]
+                [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄",callback_data="start", style=enums.ButtonStyle.PRIMARY)]
             ])
         )
 
